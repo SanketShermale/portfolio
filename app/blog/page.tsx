305a179e8bdbd0cc8c2b43a16b8d1ce254ac335b
sanketshermale <sanketshermale9@gmail.com>
2026-03-10 21:51:10 +0530

    fix blog page build

---
+++ app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <main style={{ padding: "40px" }}>
+            <h1>Blog</h1>
+            <p>This is my blog page.</p>
+        </main>
+    );
+}
